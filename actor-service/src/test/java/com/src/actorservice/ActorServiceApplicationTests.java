97bd1c8f26d94c422e99d53a8ce471c7ad8a9531
Aleš Ružić <ales.ruzic@hiwe-it.com>
2023-10-12 22:00:37 +0200

    init

---
+++ actor-service/src/test/java/com/src/actorservice/ActorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.actorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
